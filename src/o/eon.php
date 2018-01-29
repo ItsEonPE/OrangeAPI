b43ffb5f09c2d07b91b66a90316e53a1e31f6ee3
ItsEonPE <31456550+ItsEonPE@users.noreply.github.com>
2018-01-29 14:21:18 -0800

    Update eon.php

--- src/o/eon.php
+++ src/o/eon.php
@@ -3,7 +3,7 @@ namespace o;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\TextFormat;
 use pocketmine\event\Listener;
-class LoaderAPI extends PluginBase implements Listener {
+class eon extends PluginBase implements Listener {
     public function onEnable(){
       $this->getLogger()->info("ORANGEPLUGINS LOADED");
         
